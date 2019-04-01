eb2ef764a5f3d8dd7a6e663c42ec60c56a0784b9
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-01 10:08:19 +0530

    Create the board

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.css';
+// import App from './App';
+
+const table = (
+  <table className="board" id="board">
+    <tbody>
+      <tr>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+        <td>0</td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+ReactDOM.render(table, document.getElementById('root'));
