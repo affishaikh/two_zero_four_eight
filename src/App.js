2c2901496d6d30559fbfc0c9ba3780e42bfada06
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-02 16:00:35 +0530

    Extract function from Table component

--- src/App.js
+++ src/App.js
@@ -3,54 +3,44 @@ import Game from './models/Game';
 import './main.css';
 import Table from './Table';
 
-const move = function(game, event) {
-  if (event.code === 'ArrowLeft') {
-    game.leftMove();
-  }
-
-  if (event.code === 'ArrowUp') {
-    game.upMove();
-  }
-
-  if (event.code === 'ArrowRight') {
-    game.rightMove();
-  }
-
-  if (event.code === 'ArrowDown') {
-    game.downMove();
-  }
-  putValuesInBoard(game);
-};
-
-const putValuesInBoard = function(game) {
-  const cells = document.getElementsByTagName('td');
-  const board = game.getBoard();
-
-  let index = 0;
-  for (let row = 0; row < board.length; row++) {
-    for (let column = 0; column < board.length; column++) {
-      cells[index].innerText = board[row][column];
-      cells[index].className = 'color-' + board[row][column];
-      index++;
-    }
-  }
-};
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { game: new Game() };
+    const game = new Game();
+    game.startGame();
+    this.state = { game, board: game.getBoard() };
+  }
+
+  move(event) {
+    const {game} = this.state;
+    if (event.code === 'ArrowLeft') {
+      game.leftMove();
+    }
+
+    if (event.code === 'ArrowUp') {
+      game.upMove();
+    }
+
+    if (event.code === 'ArrowRight') {
+      game.rightMove();
+    }
+
+    if (event.code === 'ArrowDown') {
+      game.downMove();
+    }
+
+    this.setState(() => ({
+      board: this.state.game.getBoard()
+    }));
   }
 
   componentDidMount() {
-    this.state.game.startGame();
     const body = document.querySelector('body');
-    body.onkeydown = move.bind(null, this.state.game);
-    putValuesInBoard(this.state.game);
+    body.onkeydown = this.move.bind(this);
   }
 
   render() {
-    return <Table />;
+    return <Table board={this.state.board} />;
   }
 }
 
--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,38 @@
 import React, { Component } from 'react';
 import './main.css';
 
+const Data = function(props) {
+  const { element } = props;
+  return <td className={'color-' + element}>{element}</td>;
+};
+
+const Row = function(props) {
+  const {row} = props;
+  return (
+    <tr>
+      {row.map(element => (
+        <Data element={element} />
+      ))}
+    </tr>
+  );
+};
+
+const Body = function(props) {
+  const {board} = props;
+  return (
+    <tbody>
+      {board.map(row => (
+        <Row row={row} />
+      ))}
+    </tbody>
+  );
+};
+
 class Table extends Component {
   render() {
     return (
       <table className="board" id="board">
-        <tbody>
-          <tr>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-          <tr>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-        </tbody>
+        <Body board={this.props.board} />
       </table>
     );
   }
