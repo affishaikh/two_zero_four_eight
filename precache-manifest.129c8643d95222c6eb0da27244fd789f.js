160e413301103dc404be59364bd9799c7742b4fe
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2019-04-03 09:17:51 +0530

    Updates

---
+++ precache-manifest.129c8643d95222c6eb0da27244fd789f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08f1abaa65980456ec0e",
+    "url": "/two_zero_four_eight/static/js/runtime~main.ca33f647.js"
+  },
+  {
+    "revision": "f81fe81d6ac44eb594c0",
+    "url": "/two_zero_four_eight/static/js/main.4dc8e8da.chunk.js"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/two_zero_four_eight/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "f81fe81d6ac44eb594c0",
+    "url": "/two_zero_four_eight/static/css/main.1119302f.chunk.css"
+  },
+  {
+    "revision": "7666242b022017fcf40857d16a6cd90f",
+    "url": "/two_zero_four_eight/index.html"
+  }
+];
